c4086af9b12be27d2fdc7d10b946d5ee30a23221
Jordan Dalton <jordan.8474@gmail.com>
2017-04-22 14:16:09 +0100

    Fixed essentialmode needing to be above in YAML
    Before the "essentialmode" needed to be above "fivem-loadouts" in the YAML file.. This hopefully fixes that.

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,8 @@
--- Plugin made by Havoc for identityrp.co.uk
+dependency "essentialmode"
+
 server_scripts {
     'loadout/loadout.lua',
     'loadout/server.lua'
 }
+
 client_script 'loadout/client.lua'
