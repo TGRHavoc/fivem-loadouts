5ad0d9a41f14ee26038e51c427815decd258470a
Jordan Dalton <jordan.8474@gmail.com>
2017-04-22 17:50:26 +0100

    Tabs to spaces

--- loadout/client.lua
+++ loadout/client.lua
@@ -5,140 +5,140 @@ local seedSet = false
 function initValids()
     local am = 0
     for i = 0,GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 3) do
-    	if IsPedComponentVariationValid(GetPlayerPed(-1), 3, i, 2) then
-    		am = am + 1
-    		validTorso[am] = i
-    	end
+        if IsPedComponentVariationValid(GetPlayerPed(-1), 3, i, 2) then
+            am = am + 1
+            validTorso[am] = i
+        end
     end
     am = 0
     for i =0, GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 7) do
-    	if IsPedComponentVariationValid(GetPlayerPed(-1), 7, i, 2) then
-    		am = am + 1
-    		validUnder[am] = i
-    	end
+        if IsPedComponentVariationValid(GetPlayerPed(-1), 7, i, 2) then
+            am = am + 1
+            validUnder[am] = i
+        end
     end
 end
 
 local options = {
-	{
-		id = 2,
-		name = "hair",
-		t = 'drawable',
-		max = function()
-			return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 2)
-		end
-	},
-	{
-		id = 2,
-		t = 'texture',
-		name = "haircolour",
-		max = function()
-			return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 2, GetPedDrawableVariation(GetPlayerPed(-1), 2)) - 1
-		end
-	},
-	{
-		id = 4, -- old = 3
-		name = "torso",
-		t = 'drawable',
-		max = function()
-			local am = 0
-			for i = 0,GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 3) do
-				if IsPedComponentVariationValid(GetPlayerPed(-1), 3, i, 2) then
-					am = am + 1
-					validTorso[am] = i
-				end
-			end
-			return am
-		end
-	},
-	{
-		id = 3,
-		name = "torsotexture",
-		t = 'texture',
-		max = function()
-			return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 3, GetPedDrawableVariation(GetPlayerPed(-1), 3)) - 1
-		end
-	},
-	{
-		id = 6, -- old 11
-		name = "torsoextra",
-		t = 'drawable',
-		max = function()
-			return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 11)
-		end
-	},
-	{
-		id = 11,
-		name = "torsoextratexture",
-		t = 'texture',
-		max = function()
-			return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 11, GetPedDrawableVariation(GetPlayerPed(-1), 11)) - 1
-		end
-	},
-	{
-		id = 8, -- old = 4
-		name = "pants",
-		t = 'drawable',
-		max = function()
-			return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 4)
-		end
-	},
-	{
-		id = 4,
-		name = "pantscolour",
-		t = 'texture',
-		max = function()
-			return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 4, GetPedDrawableVariation(GetPlayerPed(-1), 4)) - 1
-		end
-	},
-	{
-		id = 10, -- old = 6
-		name = "shoes",
-		t = 'drawable',
-		max = function()
-			return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 6)
-		end
-	},
-	{
-		id = 6,
-		name = "shoescolour",
-		t = 'texture',
-		max = function()
-			return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 6, GetPedDrawableVariation(GetPlayerPed(-1), 6)) - 1
-		end
-	},
-	{
-		id = 7, -- 11
-		name = "bodyaccessory",
-		t = 'drawable',
-		max = function()
-			local am = 0
-			for i =0, GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 7) do
-				if IsPedComponentVariationValid(GetPlayerPed(-1), 7, i, 2) then
-					am = am + 1
-					validUnder[am] = i
+    {
+        id = 2,
+        name = "hair",
+        t = 'drawable',
+        max = function()
+            return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 2)
+        end
+    },
+    {
+        id = 2,
+        t = 'texture',
+        name = "haircolour",
+        max = function()
+            return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 2, GetPedDrawableVariation(GetPlayerPed(-1), 2)) - 1
+        end
+    },
+    {
+        id = 4, -- old = 3
+        name = "torso",
+        t = 'drawable',
+        max = function()
+            local am = 0
+            for i = 0,GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 3) do
+                if IsPedComponentVariationValid(GetPlayerPed(-1), 3, i, 2) then
+                    am = am + 1
+                    validTorso[am] = i
+                end
+            end
+            return am
+        end
+    },
+    {
+        id = 3,
+        name = "torsotexture",
+        t = 'texture',
+        max = function()
+            return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 3, GetPedDrawableVariation(GetPlayerPed(-1), 3)) - 1
+        end
+    },
+    {
+        id = 6, -- old 11
+        name = "torsoextra",
+        t = 'drawable',
+        max = function()
+            return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 11)
+        end
+    },
+    {
+        id = 11,
+        name = "torsoextratexture",
+        t = 'texture',
+        max = function()
+            return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 11, GetPedDrawableVariation(GetPlayerPed(-1), 11)) - 1
+        end
+    },
+    {
+        id = 8, -- old = 4
+        name = "pants",
+        t = 'drawable',
+        max = function()
+            return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 4)
+        end
+    },
+    {
+        id = 4,
+        name = "pantscolour",
+        t = 'texture',
+        max = function()
+            return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 4, GetPedDrawableVariation(GetPlayerPed(-1), 4)) - 1
+        end
+    },
+    {
+        id = 10, -- old = 6
+        name = "shoes",
+        t = 'drawable',
+        max = function()
+            return GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 6)
+        end
+    },
+    {
+        id = 6,
+        name = "shoescolour",
+        t = 'texture',
+        max = function()
+            return GetNumberOfPedTextureVariations(GetPlayerPed(-1), 6, GetPedDrawableVariation(GetPlayerPed(-1), 6)) - 1
+        end
+    },
+    {
+        id = 7, -- 11
+        name = "bodyaccessory",
+        t = 'drawable',
+        max = function()
+            local am = 0
+            for i =0, GetNumberOfPedDrawableVariations(GetPlayerPed(-1), 7) do
+                if IsPedComponentVariationValid(GetPlayerPed(-1), 7, i, 2) then
+                    am = am + 1
+                    validUnder[am] = i
 
-				end
-			end
-			return am
-		end
-	},
-	{
-		id = 8, -- 12
-		name = "undershirt",
-		t = 'drawable',
-		max = function()
-			return GetNumberOfPedDrawableVariations(GetPlayerPed(-1),  8)
-		end
-	},
-	{
-		id = 9, -- 13
-		name = "armor",
-		t = 'drawable',
-		max = function()
-			return GetNumberOfPedDrawableVariations(GetPlayerPed(-1),  9)
-		end
-	}
+                end
+            end
+            return am
+        end
+    },
+    {
+        id = 8, -- 12
+        name = "undershirt",
+        t = 'drawable',
+        max = function()
+            return GetNumberOfPedDrawableVariations(GetPlayerPed(-1),  8)
+        end
+    },
+    {
+        id = 9, -- 13
+        name = "armor",
+        t = 'drawable',
+        max = function()
+            return GetNumberOfPedDrawableVariations(GetPlayerPed(-1),  9)
+        end
+    }
 }
 
 
@@ -192,9 +192,9 @@ end)
 
 RegisterNetEvent("loadout:position")
 AddEventHandler("loadout:position", function(position)
-	if position == nil then
-		return
-	end
+    if position == nil then
+        return
+    end
     SetEntityCoords(GetPlayerPed(-1), position.x, position.y, position.z)
 end)
 
